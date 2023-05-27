ca326fafbc23d99b928bf970bde88616f2d9bc6c
VolaxYT <volax6600mailpro@gmail.com>
2023-05-27 23:14:25 +0200

    first commit

---
+++ desktop/src/fr/volax/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package fr.volax;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import fr.volax.VolaxGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(120);
+		config.setTitle("VolaxGame");
+		new Lwjgl3Application(new VolaxGame(), config);
+	}
+}
---
+++ ios/src/fr/volax/IOSLauncher.java
@@ -0,0 +1,22 @@
+package fr.volax;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import fr.volax.VolaxGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new VolaxGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
